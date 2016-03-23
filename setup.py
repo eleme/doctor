dcfd8f5e70f0023a0ba7fd157c69010825cff63d
damnever <dxc.wolf@gmail.com>
2016-03-23 11:37:00 +0800

    complete setup.py

--- setup.py
+++ setup.py
@@ -5,18 +5,20 @@ import re
 from setuptools import setup
 
 
-version = ''
-with open('doctor/__init__.py', 'r') as f:
-        version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
-                            f.read(), re.M).group(1)
+with open('doctor/__init__.py', 'rb') as f:
+    version = re.search(r'__version__\s*=\s*[\'"]([^\'"]*)[\'"]',
+                        f.read(), re.M).group(1)
+
+with open('./README.md', 'rb') as f:
+    description = f.read()
 
 
 setup(
     name='doctor',
     version=version,
-    description="",
-    url='',
-    author='Eleme Dev',
-    author_email='',
+    description=description,
+    url='https://github.com/eleme/doctor',
+    author='WangChao',
+    author_email='hit9@ele.me, xiangyu.wang@ele.me',
     packages=['doctor'],
 )
